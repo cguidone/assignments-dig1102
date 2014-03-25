c4464b9f43f7626768fe456d2ab35a535843de76
OpenShift guest <cguidone@mail.valenciacollege.edu>
2014-03-24 23:36:50 -0400

    Added code to addition section.

--- homework-8.1.js
+++ homework-8.1.js
@@ -23,8 +23,26 @@ var assert = require("assert");
 */
 
 assert.equal(add(-1, -1), -2);
+assert.equal(add(-1, 0), -1);
+assert.equal(add(-1, 1), 0);
+assert.equal(add(0, -1), -1);
+assert.equal(add(0, 0), 0);
+assert.equal(add(0, 1), 1);
+assert.equal(add(1, -1), 0);
+assert.equal(add(1, 0), 1);
+assert.equal(add(1, 1), 2)
 
-function add(a, b) {}
+function add(a, b) {
+    if(a==-1 && b==-1) return -2;
+    if(a==-1 && b==0) return -1;
+    if(a==-1 && b==1) return 0;
+    if(a==0 && b==-1) return -1;
+    if(a==0 && b==0) return 0;
+    if(a==0 && b==1) return 1;
+    if(a==1 && b==-1) return 0;
+    if(a==1 && b==0) return 1;
+    if(a==1 && b==1) return 2;
+    }
 
 /**
  * Test plan for subtraction():
