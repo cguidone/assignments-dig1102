e802ed18f7dc3bfabb556bfedf372162c467f983
OpenShift guest <cguidone@mail.valenciacollege.edu>
2014-03-25 23:03:21 -0400

    Deleted code.

--- homework-8.1.js
+++ homework-8.1.js
@@ -22,27 +22,7 @@ var assert = require("assert");
  *   +1 |  +1 |  +2
 */
 
-assert.equal(add(-1, -1), -2);
-assert.equal(add(-1, 0), -1);
-assert.equal(add(-1, 1), 0);
-assert.equal(add(0, -1), -1);
-assert.equal(add(0, 0), 0);
-assert.equal(add(0, 1), 1);
-assert.equal(add(1, -1), 0);
-assert.equal(add(1, 0), 1);
-assert.equal(add(1, 1), 2)
 
-function add(a, b) {
-    if(a==-1 && b==-1) return -2;
-    if(a==-1 && b==0) return -1;
-    if(a==-1 && b==1) return 0;
-    if(a==0 && b==-1) return -1;
-    if(a==0 && b==0) return 0;
-    if(a==0 && b==1) return 1;
-    if(a==1 && b==-1) return 0;
-    if(a==1 && b==0) return 1;
-    if(a==1 && b==1) return 2;
-    }
 
 /**
  * Test plan for subtraction():
