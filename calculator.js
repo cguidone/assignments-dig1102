fcca227c837cfcfdc4a271be21d67bfe9c7eb3c9
OpenShift guest <cguidone@mail.valenciacollege.edu>
2014-03-31 23:26:47 -0400

    Start calculator.js and calculator-test.js

---
+++ calculator.js
@@ -0,0 +1,30 @@
+module.exports = {
+    
+    add: function add (a, b) {
+        
+    },
+    
+    subtract: function subtract (a, b) {
+        
+    },
+
+    multiply: function multiply (a, b) {
+        if( a == -1 && b == -1) return +1;
+        if( a == -1 && b == +0) return +0;
+        if( a == -1 && b == +1) return -1;
+        if( a == +0 && b == -1) return +0;
+        if( a == +0 && b == +0) return +0;
+        if( a == +0 && b == +1) return -0;
+        if( a == +1 && b == -1) return -1;
+        if( a == +1 && b == +0) return +0;
+        return +1;
+    },
+
+    divide: function divide(a, b) {
+        if ( a == -1 && b == -1) return +1;
+        if ( a == -1 && b == +0) return null;
+        if ( a == -1 && b == +1) return -1;
+        if ( a == +0 && b == -1) return +0;
+        if ( a == +0 && b == +0) return null;
+    }
+};
