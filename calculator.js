b5d858db12821a68526442f2fd5c75ae196fec1f
OpenShift guest <cguidone@mail.valenciacollege.edu>
2014-04-02 23:55:50 -0400

    Completed divide test.

--- calculator.js
+++ calculator.js
@@ -43,5 +43,7 @@ module.exports = {
         if ( a == +0 && b == -1) return +0;
         if ( a == +0 && b == +0) return null;
         if ( a == +0 && b == +1) return +0;
+        if ( a == +1 && b == -1) return -1;
+        if ( a == +1 && b == +1) return +1;
     }
 };
