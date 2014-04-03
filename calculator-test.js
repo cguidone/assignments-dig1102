17db3b4c40375a11a5f58de83e447abc38b985c2
OpenShift guest <cguidone@mail.valenciacollege.edu>
2014-04-02 23:49:15 -0400

    Added multiplication tests and started on divide.

--- calculator-test.js
+++ calculator-test.js
@@ -104,6 +104,13 @@ tests.forEach(function(row){
     [ +1, +1, +1 ],
  ];
  
+ tests.forEach(function(row) {
+    var a = row[0],
+        b = row[1],
+        c = row[2];
+    assertEqual(calculator.multiply(a, b), c, "multiply: fail on " + row);
+ });
+ 
   /**
  * Test plan for division():
  * 
@@ -131,3 +138,10 @@ tests.forEach(function(row){
     [ +1, +0, null ],
     [ +1, +1, +1 ],
  ];
+ 
+ tests.forEach(function(row) {
+    var a = row[0],
+        b = row[1],
+        c = row[2];
+    assertEqual(calculator.divide(a, b), c, "divide: fail on " + row);
+ });
--- calculator.js
+++ calculator.js
@@ -42,5 +42,6 @@ module.exports = {
         if ( a == -1 && b == +1) return -1;
         if ( a == +0 && b == -1) return +0;
         if ( a == +0 && b == +0) return null;
+        if ( a == +0 && b == +1) return +0;
     }
 };
