c5563eb3dbf4e514325dc449e71bfe5f1d72b774
OpenShift guest <cguidone@mail.valenciacollege.edu>
2014-04-06 17:08:55 -0400

    Array created, and tested to make sure all doors are closed.

--- hallway-test.js
+++ hallway-test.js
@@ -1,6 +1,18 @@
 var assert = require('assert'),
     hallway = require('./hallway.js');
     
+/** Program
+ * There is a hallway with 100 doors.
+ * All doors are closed to start.
+ * 1. Walk down the hallway and open every door.
+ * 2. Walk down the hallway again and close every other door.
+ * 3. Walk down the hallway again and examine every third door,
+ * if the door is open, close it. If it is closed, open it.
+ * 4. Walk down the hallway again and examine every fourth door,
+ * if the door is open close it. If it is closed, open it.
+ * What is the state of the hallway at the end?
+*/
+    
 /**
  * First test to make sure all doors in Hallway are closed
  * Door #  | open(1)/closed(-1)
@@ -14,13 +26,19 @@ var assert = require('assert'),
  *      8  | -1
  *      9  | -1
  *      10 | -1
- */
+*/
  
 assert(hallway);
  
 assert(hallway.doors);
 
-assert.equal(hallway.doors.length, 10);
+// Test length of array doors.
+assert.equal(hallway.doors.length, 10, 'Error hallway is not equal to length specified.');
 
-// for (var a = 0; a <= hallway.doors.length; a++);
-//    assert.equal(hallway.doors[a], -1, 'Error on door ' + a);
+// Test to make sure every door is closed at the start.
+for (var a = 0; a < hallway.doors.length; a++) {
+assert.equal(hallway.doors[a], -1, 'Error on door ' + a);
+}
+
+// var a=1;
+// assert.equal(hallway.open(a), 1, "Error on door");
